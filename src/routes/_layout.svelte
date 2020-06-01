de1dfe262f88148ad5f2b2671b1f774582f2358b
Edward d'Albon <edward.partenie@gmail.com>
2020-06-01 22:35:34 +0300

    add missing _layout.svelte

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,24 @@
+<style global>
+	@font-face {
+		font-family: Whitney;
+		font-weight: 400;
+		font-display: swap;
+		src: url(/fonts/Whitney-400.woff) format('woff');
+	}
+	@font-face {
+		font-family: Whitney;
+		font-weight: 500;
+		font-display: swap;
+		src: url(/fonts/Whitney-500.woff) format('woff');
+	}
+
+	html {
+		background-color: #2f3136;
+	}
+	body {
+		font-family: Whitney, Helvetica Neue, Helvetica, Arial, sans-serif;
+		text-rendering: optimizeLegibility;
+	}
+</style>
+
+<slot />
